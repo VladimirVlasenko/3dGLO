1767f0a3246672208954626fb106016aec5c12ff
Vladimir <79533330011@ya.ru>
2020-03-02 17:31:31 +0300

    20й урок основное задание

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         body.addEventListener('click', (event) => {
             let target = event.target;
-            if (target.closest('.menu') || target.matches('a.menuLi')  || target.matches('.close-btn')) {
+            if (target.closest('.menu') || target.matches('a.menuLi, .close-btn')) {
                 handlerMenu();
             } else if (menu.classList.contains('active-menu') && !target.closest('.menu')) {
                 handlerMenu();
@@ -110,7 +110,9 @@ window.addEventListener('DOMContentLoaded', function() {
                 popup.style.display = 'none';
             } else {
                 target = target.closest('.popup-content');
-                if(!target) popup.style.display = 'none';
+                if(!target) {
+                    popup.style.display = 'none';
+                }
             }
         });
     };
@@ -146,5 +148,101 @@ window.addEventListener('DOMContentLoaded', function() {
         });     
     };
     tabs();
+    // слайдер
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+              btn = document.querySelectorAll('.portfolio-btn'),
+              slider = document.querySelector('.portfolio-content');
+              let dot;
+              
+              
+        let ulDots = document.querySelector('.portfolio-dots');
+        let createDots = () => {
+            for(let i = 0; i < slide.length; i++) {
+                let li = document.createElement('li');
+                li.classList.add('dot');
+                ulDots.appendChild(li); 
+            }
+            dot = document.querySelectorAll('.dot');
+            dot[0].classList.add('dot-active');
+            console.log(dot);
+        };
+        createDots();
+
+
+        let currentSlide = 0;
+        let interval;
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active' );
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if(currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active' );
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+        const startSlide = (time = 3000) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+            
+
+            if(!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+            
+            prevSlide(slide, currentSlide, 'portfolio-item-active' );
+            prevSlide(dot, currentSlide, 'dot-active');
+
+            if (target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if(target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if (target.matches('.dot')) {
+                dot.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            if (currentSlide < 0) {
+                currentSlide = slide.length - 1 ;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active' );
+            nextSlide(dot, currentSlide, 'dot-active');
+        });
+        slider.addEventListener('mouseover', (event) => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+        slider.addEventListener('mouseout', (event) => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                startSlide();
+            }
+        });
+
+        startSlide(2000);
+
+
+    };
+    slider();
 
 });
